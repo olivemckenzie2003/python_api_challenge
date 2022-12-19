024853123f50cac6ebef3faa5f969599ef554452
Olive Mckenzie <olivemckenzie2003@yahoo.com>
2022-12-19 17:42:05 +0000

    Add files via upload

--- WeatherPy/Starter_Code (12)/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code (12)/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2d38a4e72ed435ead3a6aeb22ef47baf"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d35dbaf5a0ca4187969f8fe81aaa4f3f"
